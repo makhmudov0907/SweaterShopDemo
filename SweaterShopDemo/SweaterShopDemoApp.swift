68894ce9f12d87b2c7b1a58cfa9756cc606d9509
makhmudov0907 <71522647+makhmudov0907@users.noreply.github.com>
2022-02-02 16:14:45 +0500

    Initial Commit

---
+++ SweaterShopDemo/SweaterShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopDemoApp.swift
+//  SweaterShopDemo
+//
+//  Created by Mirshod Makhmudov on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
